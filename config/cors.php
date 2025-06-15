6e32cf1e37f3e93199df16985bc8c716c3830f0e
fsdio <klikrahmatsunjani@gmail.com>
2025-06-15 22:13:18 +0700

    fix(cors): update allowed origins and domains for production

--- config/cors.php
+++ config/cors.php
@@ -19,14 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter([
-        env('CORS_ALLOWED_ORIGINS'),
-        env('APP_URL'),
-        'http://localhost:8000',
-        'http://localhost:5173',
-        'http://127.0.0.1:8000',
-        'http://127.0.0.1:5173',
-    ]),
+    'allowed_origins' => [
+        'https://howlson.onrender.com',
+    ],
 
     'allowed_origins_patterns' => [
         '*.ngrok.io',
